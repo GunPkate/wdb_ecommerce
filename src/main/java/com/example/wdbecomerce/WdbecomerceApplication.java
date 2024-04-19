250612ba85c69de055327fb2349a655e6185c972
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-04-19 14:37:11 -0700

    initialize spring boot

---
+++ src/main/java/com/example/wdbecomerce/WdbecomerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.wdbecomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WdbecomerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WdbecomerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wdbecomerce/WdbecomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wdbecomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WdbecomerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
